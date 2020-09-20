f01a4fe53339c2fc7a64e5ed598dfcaa25a7571d
JokerTM <rcmgn@yandex.ru>
2020-09-20 15:28:01 +0000

    Test for and while

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello world");
+int main() {
+    printf("Hello world!!!\n");
+
+/*    for (int i = 0; i < 10; i++) {
+	    printf("counter: %d\n", i);
+    }*/
+
+    int i = 0;
+    while (i < 10) {
+	    printf("counter: %d\n", i);
+	    i++;
+    }
+
+    return 0;
 }
 
